301eadfd9dde7349ff3394a8c7202bc5f8945d9c
Lowell Thoerner <lowellashton@gmail.com>
2023-01-11 13:57:51 -0800

    Optimized get_bit_permutations() to return a Range instead of a Vec.

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,11 @@ fn main() {
     Expression::parse(&args[1]);
 }
 
-fn get_bit_permutations(bits: u8) -> Vec<u8> {
-    let mut permutations = Vec::new();
+use std::ops::Range;
 
-    for i in 0..(1 << bits) {
-        permutations.push(i);
-    }
-
-    permutations
+// Gets all possible permutations of a given number of bits
+fn get_bit_permutations(bits: u8) -> Range<u8> {
+    0..(1 << bits)
 }
 
 #[cfg(test)]
@@ -30,13 +27,9 @@ mod tests {
 
     #[test]
     fn test_get_bit_permutations() {
-        assert_eq!(get_bit_permutations(0), vec![0]);
-        assert_eq!(get_bit_permutations(1), vec![0, 1]);
-        assert_eq!(get_bit_permutations(2), vec![0, 1, 2, 3]);
-        assert_eq!(get_bit_permutations(3), vec![0, 1, 2, 3, 4, 5, 6, 7]);
-        assert_eq!(
-            get_bit_permutations(4),
-            vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-        );
+        assert_eq!(get_bit_permutations(0).collect::<Vec<u8>>(), vec![0]);
+        assert_eq!(get_bit_permutations(1).collect::<Vec<u8>>(), vec![0, 1]);
+        assert_eq!(get_bit_permutations(2).collect::<Vec<u8>>(), vec![0, 1, 2, 3]);
+        assert_eq!(get_bit_permutations(3).collect::<Vec<u8>>(), vec![0, 1, 2, 3, 4, 5, 6, 7]);
     }
 }
